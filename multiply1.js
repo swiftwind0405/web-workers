b133c9b5388fb9fff78b81ce19a5d2d41edef84b
StanleyWind <swiftwind0405@gmail.com>
2020-12-07 17:26:51 +0800

    update

--- multiply1.js
+++ multiply1.js
@@ -1,22 +1,17 @@
 // 使用worker之前需要检测当前环境支持不支持
 if (window.Worker) {
   // 创建一个专用worker
-  var myWorker = new Worker("worker1.js");
+  var myWorker = new Worker("dedicated-worker.js");
 
   // 拿到DOM
-  var first = document.querySelector('#number1');
-  var second = document.querySelector('#number2');
-  var result = document.querySelector('.result1');
+  var first = document.querySelector("#number1");
+  var second = document.querySelector("#number2");
+  var result = document.querySelector(".result1");
 
   // 发送消息事件
   first.onchange = function () {
-    if (Number(first.value) > 100) {
-      console.warn("主进程关闭worker");
-      myWorker.terminate();
-    } else {
-        myWorker.postMessage([first.value, second.value]);
-        console.log("first消息发送到worker");
-    }
+    myWorker.postMessage([first.value, second.value]);
+    console.log("first消息发送到worker");
   };
   second.onchange = function () {
     myWorker.postMessage([first.value, second.value]);
